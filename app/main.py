0a7b9e7d8d7bfed47c2e7113e6a7a261a6cc14ab
B Sanjiv <bp2411@srmist.edu.in>
2026-07-01 20:46:56 +0530

    fix: allow Vercel frontend in CORS

--- app/main.py
+++ app/main.py
@@ -10,7 +10,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://fluent-iq-mu.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
